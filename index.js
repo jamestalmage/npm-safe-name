954685f605507b2b8a9cbd9cbbfc94f7bce8fc50
James Talmage <james@talmage.io>
2015-07-09 17:59:23 -0400

    change .editorconfig and reformat code

--- index.js
+++ index.js
@@ -13,38 +13,38 @@ var NAME = /^[a-z0-9\-~][a-z0-9\-\._~]*$/;
 module.exports = check;
 
 function check(name, scope) {
-	if (scope) {
-		if (!NAME.test(name)) return null;
+  if (scope) {
+    if (!NAME.test(name)) return null;
 
-		scope = SCOPE.exec(scope);
-		if (!scope) return null;
-		scope = scope[1];
+    scope = SCOPE.exec(scope);
+    if (!scope) return null;
+    scope = scope[1];
 
-		return (name.length + scope.length < 212 || null) && new NpmPackageName(scope, name);
-	} else {
-		var match = (name.length < 214 || null) && FULL_NAME.exec(name);
-		return match && new NpmPackageName(match[1], match[2]);
-	}
+    return (name.length + scope.length < 212 || null) && new NpmPackageName (scope, name);
+  } else {
+    var match = (name.length < 214 || null) && FULL_NAME.exec(name);
+    return match && new NpmPackageName(match[1], match[2]);
+  }
 }
 
 module.exports.validate = function (name, scope) {
-	var result = check(name, scope);
-	if (result) return result;
+  var result = check(name, scope);
+  if (result) return result;
 
-	var message = (scope ? new NpmPackageName(scope, name) : name).toString();
+  var message = (scope ? new NpmPackageName(scope, name) : name).toString();
   throw new Error(message + " is not a valid package name");
 };
 
 module.exports.NpmPackageName = NpmPackageName;
 
 function NpmPackageName (scope, name) {
-	this.scope = scope || null;
-	this.name = name;
+  this.scope = scope || null;
+  this.name = name;
 }
 
 NpmPackageName.prototype.toJSON =
 NpmPackageName.prototype.toString =
-NpmPackageName.prototype.valueOf = function() {
-	var scope = this.scope;
+NpmPackageName.prototype.valueOf = function () {
+  var scope = this.scope;
   return scope ? '@' + scope + '/' + this.name : this.name;
 };
--- test/test.js
+++ test/test.js
@@ -4,115 +4,115 @@ var assert = require('assert');
 
 var isSafe = require('../');
 
-describe('npm-safe-name', function() {
+describe('npm-safe-name', function () {
 
-	it('npmSafeName(name)', function(){
-		name('good-name').shouldPass();
-		name('bad name').shouldFail();
-		name('good~name').shouldPass();
-		name('Good~name').shouldFail();
-		name('good.name').shouldPass();
-		name('.goodname').shouldFail();
-		name('_goodname').shouldFail();
-	});
+  it('npmSafeName(name)', function () {
+    name('good-name').shouldPass();
+    name('bad name').shouldFail();
+    name('good~name').shouldPass();
+    name('Good~name').shouldFail();
+    name('good.name').shouldPass();
+    name('.goodname').shouldFail();
+    name('_goodname').shouldFail();
+  });
 
-	it('npmSafeName(name, scope)', function(){
-		name('good-name', 'good-scope').shouldPass();
-		name('good-name', '@good-scope').shouldPass();
-		name('good-name', 'bad scope').shouldFail();
-		name('bad name', 'good-scope').shouldFail();
-	});
+  it('npmSafeName(name, scope)', function () {
+    name('good-name', 'good-scope').shouldPass();
+    name('good-name', '@good-scope').shouldPass();
+    name('good-name', 'bad scope').shouldFail();
+    name('bad name', 'good-scope').shouldFail();
+  });
 
-	it('npmSafeName(fullName)', function(){
-		name('@good-scope/good-name').shouldPass();
-		name('bad-scope/good-name').shouldFail();
-		name('@bad scope/good-name').shouldFail();
-		name('@good-scope/bad name').shouldFail();
-	});
+  it('npmSafeName(fullName)', function () {
+    name('@good-scope/good-name').shouldPass();
+    name('bad-scope/good-name').shouldFail();
+    name('@bad scope/good-name').shouldFail();
+    name('@good-scope/bad name').shouldFail();
+  });
 
-	it('value comparison of result', function() {
-		// you can compare NPMPackageName to String using `==` but not `===`
-		assert.equal(isSafe('good-name'),'good-name');
-		assert.notStrictEqual(isSafe('good-name'), 'good-name');
-		assert.equal(isSafe('@james/good-name'),'@james/good-name');
-		assert.notStrictEqual(isSafe('@james/good-name'), '@james/good-name');
-	});
+  it('value comparison of result', function () {
+    // you can compare NPMPackageName to String using `==` but not `===`
+    assert.equal(isSafe('good-name'), 'good-name');
+    assert.notStrictEqual(isSafe('good-name'), 'good-name');
+    assert.equal(isSafe('@james/good-name'), '@james/good-name');
+    assert.notStrictEqual(isSafe('@james/good-name'), '@james/good-name');
+  });
 
-	describe('.validate will throw if input is bad', function() {
+  describe('.validate will throw if input is bad', function () {
 
-		function testValidate(name, scope, shouldThrow) {
-			var fullName = scope ? scope + '/' + name : name;
-			it (fullName + ' will ' + (shouldThrow ? '' : 'not ') + 'throw', function(done) {
-				try {
-					isSafe.validate(name, scope);
-				} catch (e) {
-					if (shouldThrow) {
-						if (/valid package name/.test(e.toString())){
-							return done();
-						} else {
-							return done(new Error('threw an error, but bad message: ' + e))
-						}
-					}
-					return done(e);
-				}
-				if (shouldThrow) return done(new Error('should have thrown'));
-				return done();
-			});
-		}
+    function testValidate(name, scope, shouldThrow) {
+      var fullName = scope ? scope + '/' + name : name;
+      it(fullName + ' will ' + (shouldThrow ? '' : 'not ') + 'throw', function (done) {
+        try {
+          isSafe.validate(name, scope);
+        } catch (e) {
+          if (shouldThrow) {
+            if (/valid package name/.test(e.toString())) {
+              return done();
+            } else {
+              return done(new Error('threw an error, but bad message: ' + e))
+            }
+          }
+          return done(e);
+        }
+        if (shouldThrow) return done(new Error('should have thrown'));
+        return done();
+      });
+    }
 
-		testValidate('@good-scope/good-name');
-		testValidate('bad-scope/good-name', null, true);
-		testValidate('@bad scope/good-name', null, true);
-		testValidate('good-name', 'good-scope');
-		testValidate('good-name', '@good-scope');
-		testValidate('good-name', '@bad scope', true);
-		testValidate('good-name', 'bad scope', true);
-		testValidate('bad name', 'good-scope', true);
+    testValidate('@good-scope/good-name');
+    testValidate('bad-scope/good-name', null, true);
+    testValidate('@bad scope/good-name', null, true);
+    testValidate('good-name', 'good-scope');
+    testValidate('good-name', '@good-scope');
+    testValidate('good-name', '@bad scope', true);
+    testValidate('good-name', 'bad scope', true);
+    testValidate('bad name', 'good-scope', true);
 
-	});
+  });
 
-	it('result has appropriate scope and name properties', function() {
-		assert.strictEqual(isSafe('good-name').name, 'good-name');
-		assert.strictEqual(isSafe('good-name').scope, null);
-		assert.strictEqual(isSafe('@james/good-name').name, 'good-name');
-		assert.strictEqual(isSafe('@james/good-name').scope, 'james');
-		assert.strictEqual(isSafe('good-name', 'good-scope').scope, 'good-scope');
-		assert.strictEqual(isSafe('good-name', '@good-scope').scope, 'good-scope');
-	});
+  it('result has appropriate scope and name properties', function () {
+    assert.strictEqual(isSafe('good-name').name, 'good-name');
+    assert.strictEqual(isSafe('good-name').scope, null);
+    assert.strictEqual(isSafe('@james/good-name').name, 'good-name');
+    assert.strictEqual(isSafe('@james/good-name').scope, 'james');
+    assert.strictEqual(isSafe('good-name', 'good-scope').scope, 'good-scope');
+    assert.strictEqual(isSafe('good-name', '@good-scope').scope, 'good-scope');
+  });
 
-	it('long names fail', function() {
-		// proof of concept for Array.join()
-		assert.strictEqual(new Array(3).join('a'), 'aa');
-		name('aa').shouldPass();
+  it('long names fail', function () {
+    // proof of concept for Array.join()
+    assert.strictEqual(new Array(3).join('a'), 'aa');
+    name('aa').shouldPass();
 
-		var maxLengthName = new Array(214).join('b');
-		name(maxLengthName).shouldPass();
+    var maxLengthName = new Array(214).join('b');
+    name(maxLengthName).shouldPass();
 
-		var tooLongName = new Array(215).join('c');
-		name(tooLongName).shouldFail();
+    var tooLongName = new Array(215).join('c');
+    name(tooLongName).shouldFail();
 
-		var prefix = '@james/';
+    var prefix = '@james/';
     var maxWithScope = new Array(214 - (prefix.length)).join('c');
-		name(prefix + maxWithScope).shouldPass();
-		name(maxWithScope, 'james').shouldPass();
-		name(maxWithScope, '@james').shouldPass();
+    name(prefix + maxWithScope).shouldPass();
+    name(maxWithScope, 'james').shouldPass();
+    name(maxWithScope, '@james').shouldPass();
 
-		var tooLongWithScope = new Array(215 - (prefix.length)).join('c');
-		name(prefix + tooLongWithScope).shouldFail();
-		name(tooLongWithScope, 'james').shouldFail();
-		name(tooLongWithScope, '@james').shouldFail();
-	});
+    var tooLongWithScope = new Array(215 - (prefix.length)).join('c');
+    name(prefix + tooLongWithScope).shouldFail();
+    name(tooLongWithScope, 'james').shouldFail();
+    name(tooLongWithScope, '@james').shouldFail();
+  });
 });
 
-function name (name, scope) {
-	var result = isSafe.apply(null, arguments);
-	var value = result || new isSafe.NpmPackageName(scope, name);
-	return {
-		shouldPass: function() {
-			assert(result, value.toString() + ' should have passed');
-		},
-		shouldFail: function() {
-			assert.strictEqual(result, null, value.toString() + ' should have failed');
-		}
-	}
+function name(name, scope) {
+  var result = isSafe.apply(null, arguments);
+  var value = result || new isSafe.NpmPackageName(scope, name);
+  return {
+    shouldPass: function () {
+      assert(result, value.toString() + ' should have passed');
+    },
+    shouldFail: function () {
+      assert.strictEqual(result, null, value.toString() + ' should have failed');
+    }
+  }
 }
