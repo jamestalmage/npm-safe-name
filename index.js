6f3c5d17ac2e95f5c4d889f878016c695fe29394
James Talmage <james@talmage.io>
2015-07-09 15:02:54 -0400

    return an object with the name and scope if it is a valid name

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ module.exports = function (str) {
 	//http://stackoverflow.com/questions/695438/safe-characters-for-friendly-url
 	//These include uppercase and lowercase letters, decimal digits, hyphen, period, underscore, and tilde
 
-	return str.length < 214 && /^(?:@[a-z0-9\-~][a-z0-9\-\._~]*\/)?[a-z0-9\-~][a-z0-9\-\._~]*$/.test(str);
+	var match =  (str.length < 214 || null)
+		&& /^(?:@([a-z0-9\-~][a-z0-9\-\._~]*)\/)?([a-z0-9\-~][a-z0-9\-\._~]*)$/.exec(str);
+
+	return match && new NpmPackageName(match[1], match[2]);
+};
+
+function NpmPackageName(scope, name) {
+	this.scope = scope || null;
+	this.name = name;
+}
+
+NpmPackageName.prototype.toString =
+NpmPackageName.prototype.valueOf = function() {
+	var scope = this.scope;
+  return scope ? '@' + scope + '/' + this.name : this.name;
 };
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 var isSafe = require('../');
 
 describe('npm-safe-name', function() {
-	it('does what it says', function(){
+	it('pass / fail', function(){
 		name('good-name').shouldPass();
 		name('bad name').shouldFail();
 		name('good~name').shouldPass();
@@ -14,8 +14,41 @@ describe('npm-safe-name', function() {
 		name('@james/my-package').shouldPass();
 		name('james/my-package').shouldFail();
 	});
-});
 
+	it('value comparison of result', function() {
+		// you can compare NPMPackageName to String using `==` but not `===`
+		assert.equal(isSafe('good-name'),'good-name');
+		assert.notStrictEqual(isSafe('good-name'), 'good-name');
+		assert.equal(isSafe('@james/good-name'),'@james/good-name');
+		assert.notStrictEqual(isSafe('@james/good-name'), '@james/good-name');
+	});
+
+	it('result has appropriate scope and name properties', function() {
+		assert.strictEqual(isSafe('good-name').name, 'good-name');
+		assert.strictEqual(isSafe('good-name').scope, null);
+		assert.strictEqual(isSafe('@james/good-name').name, 'good-name');
+		assert.strictEqual(isSafe('@james/good-name').scope, 'james');
+	});
+
+	it('long names fail', function() {
+		// proof of concept for Array.join()
+		assert.strictEqual(new Array(3).join('a'), 'aa');
+		name('aa').shouldPass();
+
+		var maxLengthName = new Array(214).join('b');
+		name(maxLengthName).shouldPass();
+
+		var tooLongName = new Array(215).join('c');
+		name(tooLongName).shouldFail();
+
+		var prefix = '@james/';
+    var maxWithScope = prefix + new Array(214 - (prefix.length)).join('c');
+		name(maxWithScope).shouldPass();
+
+		var tooLongWithScope = prefix + new Array(215 - (prefix.length)).join('c');
+		name(tooLongWithScope).shouldFail();
+	});
+});
 
 function name (s) {
 	return {
